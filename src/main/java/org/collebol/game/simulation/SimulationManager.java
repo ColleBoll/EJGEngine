d034e87fe563b9839040d8cccbb585928e835f69
Colin Okhuijzen Mulder <contact@collebol.org>
2025-11-18 22:59:14 +0100

    WIP: path class 2

--- src/main/java/org/collebol/game/simulation/SimulationManager.java
+++ src/main/java/org/collebol/game/simulation/SimulationManager.java
@@ -2,4 +2,6 @@ package org.collebol.game.simulation;
 
 public class SimulationManager {
 
+
+
 }
--- src/main/java/org/collebol/game/simulation/path/Path.java
+++ src/main/java/org/collebol/game/simulation/path/Path.java
@@ -1,20 +1,52 @@
 package org.collebol.game.simulation.path;
 
 import org.collebol.shared.GameLocation;
-import org.collebol.shared.math.Vector2D;
+import org.collebol.shared.objects.entity.Entity;
 
 import java.util.ArrayList;
 
 public abstract class Path {
 
-    private Vector2D oldLoc;
-    private int currentLoc;
-    private Vector2D newLoc;
+    private final Entity entity;
 
-    private ArrayList<GameLocation> path;
+    private GameLocation startLoc;
+    private GameLocation endLoc;
 
-    public Path() {
+    private int currentIndex;
+    private ArrayList<GameLocation> pathSteps;
 
+    public Path(Entity entity) {
+        this.entity = entity;
+    }
+
+    public void generateNewPath() {
+        this.startLoc = new GameLocation(
+                entity.getGameLocation().getX(),
+                entity.getGameLocation().getY()
+        );
+
+        this.pathSteps = pathInstructions();
+        this.currentIndex = 0;
+
+        this.endLoc = this.pathSteps.getLast();
+    }
+
+    public GameLocation getNextLocation() {
+        if (this.pathSteps == null) return null;
+
+        GameLocation l;
+        if (this.pathSteps.size() > this.currentIndex) {
+            l = this.pathSteps.get(this.currentIndex);
+            this.currentIndex++;
+            return l;
+        } else {
+            return this.endLoc;
+        }
+    }
+
+    public GameLocation getCurrentLocation() {
+        if (this.pathSteps == null) return null;
+        return this.pathSteps.get(this.currentIndex);
     }
 
     public abstract ArrayList<GameLocation> pathInstructions();
