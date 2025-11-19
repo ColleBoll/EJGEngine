d8dbe0cff8e60da847432a45d0943a2a8c1cc289
Colin Okhuijzen Mulder <contact@collebol.org>
2025-11-19 17:28:10 +0100

    Path Generation for Entitys

--- src/main/java/org/collebol/game/simulation/SimulationManager.java
+++
@@ -1,7 +0,0 @@
-package org.collebol.game.simulation;
-
-public class SimulationManager {
-
-
-
-}
--- src/main/java/org/collebol/shared/objects/entity/Entity.java
+++ src/main/java/org/collebol/shared/objects/entity/Entity.java
@@ -2,28 +2,30 @@ package org.collebol.shared.objects.entity;
 
 import org.collebol.shared.objects.GameObject;
 import org.collebol.shared.GameLocation;
+import org.collebol.shared.simulation.path.Path;
 
 import java.util.UUID;
 
 public abstract class Entity extends GameObject {
 
-    private GameLocation gameLocation;
     private final UUID uuid;
+    private Path path;
 
     public Entity(UUID uuid){
         this.uuid = uuid;
-    }
-
-    public GameLocation getGameLocation() {
-        return this.gameLocation;
-    }
-
-    public void setGameLocation(GameLocation location) {
-        this.gameLocation = location;
+        this.path = null;
     }
 
     public UUID getUuid() {
         return uuid;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public void setPath(Path path) {
+        this.path = path;
     }
 
     public abstract void update(float DT);
